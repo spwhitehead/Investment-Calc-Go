be8ceb88ed1999930ef7eb617a5b1bb6a06e50fa
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-03-13 21:29:10 -0600

    Adds inflation calculation
    Adds a return for the "real value" of the investment, adjusted for inflation.

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
